d4c65777d3f7a9f36b0ae3cb28273bcf47234d38
Unknown <goelicheng@gmail.com>
2019-04-16 16:21:47 +0200

    menu
    Messed around with setting active and inactive ui objects

---
+++ Assets/scripts/ButtonBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public void Setbutton (string text)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+
+    }
+
+    public GameObject Home;
+    public GameObject Search;
+    public GameObject Profile;
+    public void Setactive()
+    {
+
+        // Home.gameObject.SetActive(true);
+        // Search.gameObject.SetActive(true);
+        // Profile.gameObject.SetActive(true);
+
+        if (Home.activeInHierarchy && !Search.activeInHierarchy && !Profile.activeInHierarchy)
+        {
+        Home.gameObject.SetActive(true);
+        // Search.gameObject.SetActive(false);
+        // Profile.gameObject.SetActive(false);  
+        } 
+         else if (Search.activeInHierarchy && !Home.activeInHierarchy && !Profile.activeInHierarchy)
+        {
+        Search.gameObject.SetActive(true);
+        // Home.gameObject.SetActive(false);
+        // Profile.gameObject.SetActive(false);   
+        } 
+        else if (Profile.activeInHierarchy && !Home.activeInHierarchy && !Profile.activeInHierarchy)
+        {
+       Profile.gameObject.SetActive(true);
+        // Home.gameObject.SetActive(false);
+        // Search.gameObject.SetActive(false);   
+        }
+        
+    }
+
+        public void Setinactive()
+    {
+        Home.gameObject.SetActive(false);
+        Search.gameObject.SetActive(false);
+        Profile.gameObject.SetActive(false);        
+
+    }
+}
