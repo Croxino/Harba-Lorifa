4da774374fd2d2cc2539c7302a756bbea7daecaf
Unknown <goelicheng@gmail.com>
2019-05-01 10:23:57 +0200

    I can instantiate cards
    Since setactive didnt work I tried to use instantiate so it creates new cards. Now I have to figure out the instantiate a fixed position and distance from eachother and different cards of the same city

---
+++ Assets/scripts/searchFilter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class searchFilter : MonoBehaviour
+{
+
+string filter;
+
+public InputField searchbar;
+//public InputField input;
+public GameObject cardobjects;
+public List<GameObject> cardList = new List<GameObject>();
+//public GameObject[] cards = new GameObject[0];
+
+    public void search(string filter)
+    {
+        foreach (var card in cardList)
+        {
+            if (searchbar.text == card.name)
+            {
+                Instantiate(card);
+            }
+
+        }
+        // filter = searchbar.text;
+
+
+        // if (searchbar.text == filter)
+        // {
+        //     cards = GameObject.FindGameObjectsWithTag(filter);
+        //     for (int i = 0; i < cards.Length; i++)
+        //     {
+
+        //         cards[i].SetActive(true);
+
+        //     }
+            
+        // }
+        Debug.Log(searchbar.text);
+        Debug.Log("list: " + cardobjects);
+        Debug.Log("list: " + filter);
+
+
+
+    }
+
+
+}
